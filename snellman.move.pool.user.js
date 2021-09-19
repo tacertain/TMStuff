f2b0d3b0b7a2b5971f2c721ee7cb94d0ef4ba571
Andrew Certain <certain@thecertains.com>
2021-09-19 13:09:32 -0700

    Fixup metadata

--- snellman.move.pool.user.js
+++ snellman.move.pool.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Move tile pool
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  Move the bonus/favor tile pool to the right of the cult tracks.
+// @author       https://github.com/tacertain
 // @match        https://terra.snellman.net/faction/*
 // @icon         https://www.google.com/s2/favicons?domain=snellman.net
 // @grant        none
